39046106edb6709db386fbd8d6263803cb23fafb
Victor Correa <118310294+N4G01@users.noreply.github.com>
2025-07-07 18:11:12 -0300

    Implementação 9.4 - Prim's Algorithm

--- Fad/Chapter9.lean
+++ Fad/Chapter9.lean
@@ -17,4 +17,55 @@ def weight (e : Edge) : Weight := e.2.2
 
 abbrev AdjArray := Lean.AssocList Vertex (List (Vertex × Weight))
 
+
+
+/- 9.4 Prim’s algorithm -/
+
+abbrev State := (Tree × List Edge)
+
+def add (e: Edge) (t: Tree) : Tree :=
+  let (vs, es) := t
+
+  cond (vs.contains (source e)) (target e :: vs, e :: es) (source e :: vs, e::es)
+
+
+def safeEdge (e: Edge) (t: Tree) : Bool :=
+  (nodes t).contains (source e) ≠ (nodes t).contains (target e)
+
+
+def steps (s: State) : List State :=
+  let (t,es) := s
+  (picks es).filterMap fun (e, es') => cond (safeEdge e t) (some (add e t, es')) none
+
+
+def spats (g : Graph) : List Tree :=
+  let start : State :=
+    match nodes g with
+     | []     => (([], []), [])
+     | v :: _ => (([v], []), edges g)
+
+  let done : State → Bool :=
+    fun (t, _) => (nodes t).length == (nodes g).length
+
+  let rec helper (ss : List State) (fuel: Nat): List Tree :=
+    match fuel with
+      | 0        => panic! "Never here"
+      | fuel + 1 => cond (ss.all done) (ss.map Prod.fst) (helper (concatMap steps ss) fuel) termination_by fuel
+
+  helper [start] g.1.length
+
+
+--#eval spats ([1,2,3,4],[(1,2,1), (2,3,2), (3,4,5)])
+
+
+def gstep : State → State
+  | (t, [])      => (t, [])
+  | (t, e :: es) =>
+
+    let keep (e: Edge) (s: State) : State :=
+      let (t', es') := s
+      (t', e :: es')
+
+    cond (safeEdge e t) (add e t, es) (keep e (gstep (t, es)))
+
 end Chapter9
