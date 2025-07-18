a87286ed860216612402aaab1b6a556fff42a9ca
Germano Andrade Brandão <65316456+GermanoAndrade@users.noreply.github.com>
2025-07-17 21:07:46 -0300

    Adicionar seção 12.3 (#46)
    * Adicionar seção 12.3
    
    * Add comments for section 12.1
    
    Adding comments for existing section 12.1 to improve reading.
    
    * Add part of section 12.3 code
    
    Adding initial elements of the section, as well as the
    cost funcions described.
    
    * Complete section 12.3
    
    Adding costs and greedy algorithm in basic version and
    version with cost added
    
    * Attempt to prove termination for parts
    
    Add theorem trying to prove termination por
    parts in 12.1, but the prove was not completed.
    
    * Update README.md
    
    Complete sections 12.1 and 12.3
    
    * Fix typo

--- Fad/Chapter12.lean
+++ Fad/Chapter12.lean
@@ -1,21 +1,60 @@
 import Fad.Chapter1
+import Fad.Chapter7
+import Fad.Chapter8
 
 namespace Chapter12
 open Chapter1 (concatMap)
 
+/-!
+# Chapter Info
+
+In this chapter $ 2 ∈ 3$ we will confine ourselves to just two examples.
+The first is a simple **scheduling problem**, while the second involves **breaking paragraphs into lines**.
+-/
+
+-- # Section12.1 Ways of generating partitions
+section
+
+/-- A partition of a list of type [A] has type [[A]], so a list of partitions has type
+[[[A]]]. -/
 abbrev Segment (a : Type) := List a
 abbrev Partition (a : Type) := List (Segment a)
+
 
 def splits {a : Type} : List a → List (List a × List a)
  | [] => []
  | x :: xs => ([x], xs) :: (splits xs).map fun ⟨ys, zs⟩ => (x :: ys, zs)
 
-partial def parts: List a → List (Partition a)
- | [] => [[]]
- | xs =>
-   (splits xs).flatMap fun ⟨ys, zs⟩ =>
-     (parts zs).map fun yss => ys :: yss
 
+theorem length_lt_of_cons_split {α : Type} (xs ys zs : List α)
+  (h : (ys, zs) ∈ splits xs) : zs.length < xs.length := by
+  induction xs generalizing ys zs with
+  | nil =>
+    simp [splits] at h
+  | cons x xs ih =>
+    simp only [splits, List.map, List.flatMap, List.mem_cons, List.mem_map] at h
+    cases h with
+    | inl h' =>
+      simp at h'
+      cases h'; simp_all [List.length]
+    | inr h'' =>
+      rcases h'' with ⟨p, hin, heq⟩
+      cases p with
+      | mk ys' zs' =>
+        cases heq
+        specialize ih ys' zs' hin
+        simp [List.length]
+        exact Nat.lt_succ_of_lt ih
+
+def parts {a : Type} : List a → List (Partition a)
+  | [] => [[]]
+  | xs =>
+    (splits xs).flatMap fun ⟨ys, zs⟩ =>
+      have : zs.length < xs.length := by
+        apply length_lt_of_cons_split xs ys zs
+        sorry
+      (parts zs).map fun yss => ys :: yss
+  termination_by xs => xs.length
 
 def cons (x : a) (p : Partition a) : Partition a :=
  [x] :: p
@@ -54,7 +93,106 @@ def extendr (x : a) : Partition a → List (Partition a)
 
 def parts₂ : List Nat → List (Partition Nat) :=
   List.foldl (flip (concatMap ∘ extendr)) [[]]
+end
+
+-- # Section 12.3 The paragraph problem
+section
+open Chapter1 (foldr)
+open Chapter7 (minWith)
+open Chapter8.S1 (foldrn)
+
+/-- The major constraint on paragraphs is that all lines have to fit into a specified
+width. For simplicity, we assume a single globally defined value maxWidth that
+gives the maximum width a line can possess.
+-/
+def maxWidth : Nat := 80
+
+/--The definition depends on another globally defined constant optWidth, whose value
+is at most maxWidth and which specifies the optimum width of each line of a
+paragraph.
+-/
+def optWidth : Nat := 60
+
+/-- It is assumed that a text consists of a nonempty sequence of words, each word being
+a nonempty sequence of non-space characters. A paragraph therefore consists of at
+least one line.
+-/
+abbrev Word := List Char
+abbrev Text := List Word
+abbrev Line := List Word
+abbrev Para := List Line
+
+def width (line : Line) : Nat :=
+  foldrn (fun w n => w.length + 1 + n) (fun w => w.length) line
+
+--- another way
+def aux_add (w : Word) (n : Nat) : Nat := w.length + 1 + n
+
+def width₁ : Line → Nat :=
+  foldrn aux_add List.length
+---
+
+def fits (line : Line) : Bool :=
+  width line ≤  maxWidth
+
+--- test cases
+def foo : Word := ['f', 'o', 'o']
+def bar : Word := ['b', 'a', 'r']
+def line : Line := [foo, bar]
+
+#eval width line
+#eval width₁ line
+
+--- end test cases
+
+/-- ## Cost functions -/
+
+def cost₁ (p : Para) : Nat := p.length
 
 
+def waste₂ (line : Line) : Nat := maxWidth - width line
+
+def cost₂ (p : Para) : Nat :=
+  (Chapter12.init p).map waste₂ |> List.sum
+
+def waste₃ (line : Line) : Nat :=
+  let d := optWidth - width line
+  d * d
+
+def cost₃ (p : Para) : Nat :=
+  (Chapter12.init p).map waste₃ |> List.sum
+
+def cost₄ (p : Para) : Nat :=
+  (Chapter12.init p).map waste₂ |> List.foldr max 0
+
+def cost₅ (p : Para) : Nat :=
+  (Chapter12.init p).map waste₃ |> List.foldr max 0
+
+/--  There is an obvious greedy algorithm for the paragraph problem: -/
+def add (p : Para) (w : Word) : Para :=
+  match p with
+  | [] => Chapter12.snoc w []
+  | _  =>
+    let candidates := [Chapter12.bind w p, Chapter12.snoc w p]
+    let valid := candidates.filter (λ para => fits (Chapter12.last para))
+    valid.headD []
+
+def basic_greedy (ws : Text) : Para :=
+  ws.foldl add []
+
+/-- Adding cost to the algorithm -/
+def tstep (ps : List Para) (w : Word) (cost : Para -> Nat) : List Para :=
+  match ps with
+  | [[]] => [[[w]]]
+  | _    =>
+    let newLine := Chapter7.minWith cost (ps.map (Chapter12.snoc w))
+    let sameLine := ps.map (Chapter12.bind w) |>.filter (fits ∘ Chapter12.last)
+    newLine :: sameLine
+
+def para (ws : Text) (cost : Para -> Nat) : Para :=
+  let step := λ ps w => tstep ps w cost
+  Chapter7.minWith cost (ws.foldl step [[]])
+
+end
 
 end Chapter12
