5977b35d5f7972492c4044c14dfa6f534f7f2c93
Alexandre Rademaker <arademaker@gmail.com>
2025-08-18 11:04:57 -0300

    update to Lean 4.23
    - added an instance in Chapter5 that needs to be checked.

--- Fad/Chapter5.lean
+++ Fad/Chapter5.lean
@@ -367,6 +367,10 @@ def subs (xs ys : List (a × Label)) : List (a × Pair) :=
 def switch : a × Pair → a × Pair
 | (x, (i, j)) => (-x, (j, i))
 
+instance : Ord (a × Pair) where
+  compare p q :=
+    compare p.1 q.1
+
 def sortWith (abs : List (a × Pair)) (xis yis : List (a × Label))
   : List (a × Pair) :=
   let a := Std.HashMap.ofList (qsort₂ compare abs |>.map Prod.snd |>.zipIdx)
